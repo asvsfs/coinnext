9908eb885253be1cb8c8f13704803ead75e5d391
Amir Nouri <anasvsfs@gmail.com>
2019-10-29 19:07:20 +0330

    s

--- tests/helpers/auth_helper.js
+++ tests/helpers/auth_helper.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-var _ = require("underscore");
-var request = require("supertest");
-var userData = {
-  email: "test@test.com",
-  password: "test12345_"
-};
-
-Auth = {
-  login: function (options, callback) {
-    var data = _.extend(userData, typeof(options) !== "function" ? options : {});
-    data.email_verified = typeof(options.email_verified) !== "undefined" ? options.email_verified : true;
-    var cb = typeof(options) === "function" ? options : callback;
-    global.db.User.createNewUser(data, function (err, user) {
-      if (err) {console.error(err)};
-      request(global.appConfig().app_host)
-      .post("/login")
-      .send(userData)
-      .end(function (err, res) {
-        cb(err, res.headers['set-cookie'], user, res);
-      });
-    });
-  }
-};
-
-=======
 var _ = require("underscore");
 var request = require("supertest");
 var userData = {
@@ -48,5 +22,4 @@ Auth = {
   }
 };
 
->>>>>>> 48b7149a4fb3de9f81af85fbc2b413fd8281be78
 module.exports = Auth;
